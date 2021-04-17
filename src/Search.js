b8d94729c3b44b52710b89eb8660fd2bbdcf6e6b
Sibidomaskuli <shildrisno1@gmail.com>
2021-04-17 04:32:15 +0100

    added axios for city weather search

--- src/Search.js
+++ src/Search.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState} from "react";
+import axios from 'axios';
 import "./Search.css";
 
 export default function Search() {
+  let [city, setCity] = useState(null); 
+
+  function updateCity(event) {
+    setCity(event, target, value); 
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey="61de322b4c57e14ee5306e572d84bdb5"
+  let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+  axios.get(apiUrl).then(displayForecast);      
+  }
+
+  function displayForecast(response) {
+    alert(response.data.main.temp); 
+  }
+
   return (
     <div className="Search">
-      <form id="search-city">
+      <form id="search-city" onSubmit={handleSubmit}>
         <input
           type="text"
           placeHolder="🧭Enter a city"
           autoComplete="off"
           autoFocus="off"
           id="box-contents"
+          onChange={updateCity}
         />
-        <input type="submit" id="search-button" value="search" />
+        <input type="submit" id="search-button" value="Search" />
       </form>
     </div>
   );
