90064a3627b0f5aba4995b5a99e4b786185248ac
Sibidomaskuli <shildrisno1@gmail.com>
2021-04-17 04:49:05 +0100

    tested axios function in app

--- src/Search.js
+++ src/Search.js
@@ -6,18 +6,19 @@ export default function Search() {
   let [city, setCity] = useState(null); 
 
   function updateCity(event) {
-    setCity(event, target, value); 
+    setCity(event.target.value); 
   }
 
   function handleSubmit(event) {
     event.preventDefault();
     let apiKey="61de322b4c57e14ee5306e572d84bdb5"
-  let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+    let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
   axios.get(apiUrl).then(displayForecast);      
   }
 
   function displayForecast(response) {
-    alert(response.data.main.temp); 
+    alert(Math.round(response.data.main.temp)); 
+    console.log(response.data); 
   }
 
   return (
