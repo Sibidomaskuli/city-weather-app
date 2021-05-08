938eefa378a81758c793f8645e3ba02d2358394c
Sibidomaskuli <shildrisno1@gmail.com>
2021-05-08 22:35:49 +0100

    Created DailyForecast.js & css files

--- src/DailyForecast.js
+++ src/DailyForecast.js
@@ -1,23 +1,20 @@
 import React from "react";
-import WeatherIcon from "./WeatherIcon"; 
+import WeatherIcon from "./WeatherIcon";
+import "./DailyForecast.css"; 
 
 export default function DailyForecast() {
  return (
   <div className="DailyForecast">
    <div className="row">
     <div className="col">
-     <div>
-     <div>
-       <strong>        
-        Saturday      
-      </strong>
-     </div>
-    <WeatherIcon code="09d" />
-     <div>
-       <strong>9°{" "}</strong>
-    | 7°
+     <div className="DailyForecast-day">
+      <strong>Saturday</strong>           
+     </div>     
+    <WeatherIcon code="09d" size={36} />
+     <div className="DailyForecast-temperature">
+      <strong className="DailyForecast-temperature-max">9°{" "}|{" "}</strong>
+    <span className="DailyForecast-temperature-min">7°</span>
     </div>
-     </div>
     </div>
    </div>
   </div>
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -12,7 +12,7 @@ export default function WeatherInfo(props) {
       <div className="Clearfix">
        <div className="float-left">
         <WeatherIcon
-         code={props.data.icon} />
+         code={props.data.icon} size={50} />
        </div>       
       </div>
       <Converter celsius={props.data.temperature}/>
