bd76e2fc814846c82fd3b5c5d36618241189c74e
Sibidomaskuli <shildrisno1@gmail.com>
2021-05-09 22:19:20 +0100

    Improved icons code

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -18,16 +18,11 @@ export default function WeatherInfo(props) {
       <Converter celsius={props.data.temperature} />
       
       <ul className="Forecasting">
-       <li><i class="bi bi-thermometer"></i>{" "}
+       <li><i className="bi bi-thermometer"></i>{" "}
         Pressure: {props.data.pressure} hPa</li>
-       <li><i class="bi bi-moisture"></i>{" "}
+       <li><i className="bi bi-moisture"></i>{" "}
         Humidity: {props.data.humidity} %</li>
-       <li>
-        <span>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-wind" viewBox="0 0 16 16">
-        <path d="M12.5 2A2.5 2.5 0 0 0 10 4.5a.5.5 0 0 1-1 0A3.5 3.5 0 1 1 12.5 8H.5a.5.5 0 0 1 0-1h12a2.5 2.5 0 0 0 0-5zm-7 1a1 1 0 0 0-1 1 .5.5 0 0 1-1 0 2 2 0 1 1 2 2h-5a.5.5 0 0 1 0-1h5a1 1 0 0 0 0-2zM0 9.5A.5.5 0 0 1 .5 9h10.042a3 3 0 1 1-3 3 .5.5 0 0 1 1 0 2 2 0 1 0 2-2H.5a.5.5 0 0 1-.5-.5z" />
-       </svg>{" "}
-        </span>
+       <li><i class="bi bi-wind"></i>{" "}     
         Wind: {props.data.wind} km/h</li>
       </ul>
      </div>
@@ -43,3 +38,5 @@ export default function WeatherInfo(props) {
   </div>
  );
 }
+        
+        
