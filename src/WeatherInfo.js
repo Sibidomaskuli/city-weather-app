8a66ef175953783056f410591035b790e4bf1e70
Sibidomaskuli <shildrisno1@gmail.com>
2021-05-09 21:52:29 +0100

    Added icons

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -13,14 +13,16 @@ export default function WeatherInfo(props) {
        <div className="float-left">
         <WeatherIcon
          code={props.data.icon} size={50} />
-       </div>       
+       </div>
       </div>
-      <Converter celsius={props.data.temperature}/>
+      <Converter celsius={props.data.temperature} />
       
       <ul className="Forecasting">
        <li>Atmospheric Pressure: {props.data.pressure} hPa</li>
        <li>Humidity: {props.data.humidity} %</li>
-       <li>Wind: {props.data.wind} km/h</li>
+       <li><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-wind" viewBox="0 0 16 16">
+        <path d="M12.5 2A2.5 2.5 0 0 0 10 4.5a.5.5 0 0 1-1 0A3.5 3.5 0 1 1 12.5 8H.5a.5.5 0 0 1 0-1h12a2.5 2.5 0 0 0 0-5zm-7 1a1 1 0 0 0-1 1 .5.5 0 0 1-1 0 2 2 0 1 1 2 2h-5a.5.5 0 0 1 0-1h5a1 1 0 0 0 0-2zM0 9.5A.5.5 0 0 1 .5 9h10.042a3 3 0 1 1-3 3 .5.5 0 0 1 1 0 2 2 0 1 0 2-2H.5a.5.5 0 0 1-.5-.5z" />
+       </svg>Wind: {props.data.wind} km/h</li>
       </ul>
      </div>
      <div className="col-6">
