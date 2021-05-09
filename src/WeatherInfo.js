53497b5bc089f4fff5967948474ba99fae85eaa3
Sibidomaskuli <shildrisno1@gmail.com>
2021-05-09 22:06:30 +0100

    Added more icons

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -18,11 +18,17 @@ export default function WeatherInfo(props) {
       <Converter celsius={props.data.temperature} />
       
       <ul className="Forecasting">
-       <li>Atmospheric Pressure: {props.data.pressure} hPa</li>
-       <li>Humidity: {props.data.humidity} %</li>
-       <li><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-wind" viewBox="0 0 16 16">
+       <li><i class="bi bi-thermometer"></i>{" "}
+        Pressure: {props.data.pressure} hPa</li>
+       <li><i class="bi bi-moisture"></i>{" "}
+        Humidity: {props.data.humidity} %</li>
+       <li>
+        <span>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-wind" viewBox="0 0 16 16">
         <path d="M12.5 2A2.5 2.5 0 0 0 10 4.5a.5.5 0 0 1-1 0A3.5 3.5 0 1 1 12.5 8H.5a.5.5 0 0 1 0-1h12a2.5 2.5 0 0 0 0-5zm-7 1a1 1 0 0 0-1 1 .5.5 0 0 1-1 0 2 2 0 1 1 2 2h-5a.5.5 0 0 1 0-1h5a1 1 0 0 0 0-2zM0 9.5A.5.5 0 0 1 .5 9h10.042a3 3 0 1 1-3 3 .5.5 0 0 1 1 0 2 2 0 1 0 2-2H.5a.5.5 0 0 1-.5-.5z" />
-       </svg>Wind: {props.data.wind} km/h</li>
+       </svg>{" "}
+        </span>
+        Wind: {props.data.wind} km/h</li>
       </ul>
      </div>
      <div className="col-6">
