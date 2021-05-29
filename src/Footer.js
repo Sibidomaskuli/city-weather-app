cafbadebb8008da947ec4093c45ab3f2ddd4c978
Sibidomaskuli <shildrisno1@gmail.com>
2021-05-29 01:14:25 +0100

    Updated Footer links

--- src/Footer.js
+++ src/Footer.js
@@ -5,22 +5,24 @@ export default function Footer (){
  return (
   <div className="Footer">
    <footer>
-    Brain- stormed by <em>Shirin Eldris</em> 🤖, {""}
+       <center>
+         <em>I made this</em>{" "}
     <a
      href="https://github.com/Sibidomaskuli/sunny-disposition.git"
      className="purple"
      target="_blank"
      rel="noopener noreferrer"
     >
-     Open-Sourced on GitHub,
-    </a>
+     GitHub
+    </a>🌍
     <a href="https://upbeat-fermat-86f53d.netlify.app/"
        className="green"
        target="_blank"
        rel="noopener noreferrer">
      {" "}
-     Netlify hosted
+     Netlify
     </a>
+    </center>
    </footer>
   </div>
  );
